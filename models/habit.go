b8db2275eb527c7139f80112f945480ecf62b5d8
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-15 18:03:47 -0300

    feat: add habitrecord model

--- models/habit.go
+++ models/habit.go
@@ -1,9 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Habit struct {
 	gorm.Model
-	Name string `json:"name"`
-	Goal int    `json:"goal"`
+	Name    string        `json:"name"`
+	Goal    int           `json:"goal"`
+	Records []HabitRecord `json:"records"`
+}
+
+type HabitRecord struct {
+	gorm.Model
+	HabitID uint      `json:"habitID"`
+	Date    time.Time `json:"date"`
 }
