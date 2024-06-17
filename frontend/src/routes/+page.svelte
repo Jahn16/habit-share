2c1eb6f797096705f7c07df5b5814b5564860c65
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-17 09:12:44 -0300

    feat: loading habits into page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Habit, HabitRecord } from '../models.ts';
+	export let data: { habits: Habit[] };
+	let dayNumbers = Array.from({ length: 30 }, (_, i) => i + 1);
+	let dates = dayNumbers.map((day) => `2024-06-${day}T00:00:00Z`);
+	const habitRecorded = (habit: Habit, day: string): boolean => {
+		const habitRecordedInThatDay = (record: HabitRecord): boolean => {
+			return record.date === day;
+		};
+		return habit.records.some(habitRecordedInThatDay);
+	};
+</script>
+
+<ul>
+	{#each dates as date}
+		{#each data.habits as habit}
+			{#if habitRecorded(habit, date)}
+				<li>{date}: {habit.name}</li>
+			{/if}
+		{/each}
+	{/each}
+</ul>
