197da180d21df73fafd40c8757bbd13ea5999096
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-17 08:40:40 -0300

    feat: add records to habits

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -5,11 +5,21 @@ export function load(): { habits: Habit[] } {
 		habits: [
 			{
 				name: 'Beber aqua',
-				goal: 7
+				goal: 7,
+				records: [
+					{ date: '2024-06-17T00:00:00Z' },
+					{ date: '2024-06-15T00:00:00Z' },
+					{ date: '2024-06-14T00:00:00Z' }
+				]
 			},
 			{
 				name: 'Malhar',
-				goal: 4
+				goal: 4,
+				records: [
+					{ date: '2024-06-15T00:00:00Z' },
+					{ date: '2024-06-14T00:00:00Z' },
+					{ date: '2024-06-13T00:00:00Z' }
+				]
 			}
 		]
 	};
