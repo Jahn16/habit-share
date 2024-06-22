7cb85fabc24556e99f042d4f4854a0f879dbeca0
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-22 16:15:51 -0300

    feat: use auth middleware

--- backend/main.go
+++ backend/main.go
@@ -5,13 +5,20 @@ import (
 
 	"github.com/Jahn16/habitshare/database"
 	"github.com/Jahn16/habitshare/handlers"
+
+	"os"
+
+	"github.com/Jahn16/habitshare/middlewares/googleauth"
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
-
 	db, _ := database.Setup()
+	godotenv.Load()
+	authMiddleware := googleauth.New(googleauth.Config{GoogleID: os.Getenv("AUTH_GOOGLE_ID")})
 
 	app.Get("/habits", handlers.HabitList(db))
 	app.Get("/habits/:id", handlers.HabitGet(db))
@@ -19,7 +26,8 @@ func main() {
 	app.Post("/habits/:id/record", handlers.RecordHabit(db))
 
 	app.Get("/users", handlers.UserList(db))
-	app.Post("/users", handlers.UserCreate(db))
+
+	app.Post("/users", authMiddleware, handlers.UserCreate(db))
 	app.Get("/users/:id", handlers.UserGet(db))
 
 	log.Fatal(app.Listen(":8000"))
