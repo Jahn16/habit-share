ee7c07eded5453599869aeb3cae1b9eb9f1e368e
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-21 22:00:20 -0300

    feat: add google auth

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { signIn } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
 	import type { Habit, HabitRecord } from '../models.ts';
 	import Record from '../components/record.svelte';
 	export let data: { habits: Habit[] };
@@ -12,6 +14,15 @@
 	};
 </script>
 
+<div>
+	<button on:click={signIn} />
+</div>
+<nav>
+	<img
+		src={$page.data.session?.user?.image ?? 'https://source.boringavatars.com/marble/120/'}
+		alt="User Avatar"
+	/>
+</nav>
 <ul>
 	{#each data.habits as habit}
 		<span>{habit.name}:</span>
