279cc0c79446630f05d8dd8005ca65e26c724e6b
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-22 13:16:49 -0300

    feat: add name to user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	Name    string  `json:"name"`
 	Email   string  `json:"email"`
 	Picture string  `json:"picture"`
 	Habits  []Habit `json:"habits"`
