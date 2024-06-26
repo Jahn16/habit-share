d341f42e33e5ada322677ff538ba7ee424d3d9a3
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-26 19:45:49 -0300

    feat: extend session to include accessToken

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,6 +1,27 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
+// import { DefaultSession } from '@auth/core/types';
+
+declare module '@auth/sveltekit' {
+	interface Session {
+		accessToken: string;
+	}
+}
 
 export const { handle } = SvelteKitAuth({
-	providers: [Google]
+	providers: [Google],
+	callbacks: {
+		jwt({ token, account }) {
+			if (account?.id_token) {
+				return { ...token, accessToken: account.id_token };
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			if (typeof token.accessToken === 'string') {
+				session.accessToken = token.accessToken;
+			}
+			return session;
+		}
+	}
 });
