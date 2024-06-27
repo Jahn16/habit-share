1085a8505df995b1b42528cb148286c9affb979a
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-26 21:31:13 -0300

    feat: add form to habit record

--- frontend/src/components/record.svelte
+++ frontend/src/components/record.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { Form } from '@sveltestrap/sveltestrap';
+
 	// import type { HabitRecord } from '../models';
 	export let done = false;
+	export let habitId: number;
+	export let date: string;
 </script>
 
-<input type="checkbox" checked={done} />
+<Form method="post" action="?/record">
+	<input type="hidden" value={habitId} name="habit-id" />
+	<input type="hidden" value={date} name="date" />
+	<input type="checkbox" checked={done} onChange="this.form.submit()" />
+</Form>
