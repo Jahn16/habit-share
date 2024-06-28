69eb74b8dcff033bd9639d2ed96e8356879840e6
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-27 21:58:37 -0300

    feat: add undo form

--- frontend/src/components/record.svelte
+++ frontend/src/components/record.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import { Form } from '@sveltestrap/sveltestrap';
 
-	// import type { HabitRecord } from '../models';
-	export let done = false;
+	import type { HabitRecord } from '../models';
 	export let habitId: number;
 	export let date: string;
+	export let record: HabitRecord | undefined = undefined;
 </script>
 
-<Form method="post" action="?/record">
-	<input type="hidden" value={habitId} name="habit-id" />
-	<input type="hidden" value={date} name="date" />
-	<input type="checkbox" checked={done} onChange="this.form.submit()" />
-</Form>
+{#if !record}
+	<Form method="post" action="?/record">
+		<input type="hidden" value={habitId} name="habit-id" />
+		<input type="hidden" value={date} name="date" />
+		<input type="checkbox" onChange="this.form.submit()" />
+	</Form>
+{:else}
+	<Form method="post" action="?/undo">
+		<input type="hidden" value={record.ID} name="record-id" />
+		<input type="checkbox" onChange="this.form.submit()" checked />
+	</Form>
+{/if}
