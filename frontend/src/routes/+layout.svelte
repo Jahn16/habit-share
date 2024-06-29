ef20cea6d900ab0ce16b078ed0d989c23c7411d7
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-28 22:20:34 -0300

    feat: add navbar

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import {
+		Button,
+		Nav,
+		NavItem,
+		NavLink,
+		Navbar,
+		NavbarBrand,
+		Image
+	} from '@sveltestrap/sveltestrap';
+	import { page } from '$app/stores';
+	import { signIn } from '@auth/sveltekit/client';
+</script>
+
+<Navbar color="dark" theme="dark">
+	<NavbarBrand href="/">Social Habits</NavbarBrand>
+	<Nav>
+		{#if !$page.data.session}
+			<NavItem>
+				<Button children="Login" color="primary" on:click={signIn} />
+			</NavItem>
+		{:else}
+			<NavItem>
+				<NavLink href="/users/me">
+					<Image
+						thumbnail
+						src={$page.data.session?.user?.image ?? 'https://source.boringavatars.com/marble/120/'}
+						alt="User Avatar"
+						class="img-circle"
+						style="max-width: 48px;max-height: 48px;"
+					/>
+				</NavLink>
+			</NavItem>
+		{/if}
+	</Nav>
+</Navbar>
+
+<slot></slot>
