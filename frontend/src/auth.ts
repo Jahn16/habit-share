9595176e1add69e55cac97f7db98db7c4a2861c8
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-03 19:35:40 -0300

    fix: add token rotation to auth

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,7 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import Google from '@auth/sveltekit/providers/google';
 import Auth0 from '@auth/sveltekit/providers/auth0';
-// import { DefaultSession } from '@auth/core/types';
+import { env } from '$env/dynamic/private';
 
 declare module '@auth/sveltekit' {
 	interface Session {
@@ -9,14 +8,46 @@ declare module '@auth/sveltekit' {
 	}
 }
 
-export const { handle } = SvelteKitAuth({
-	providers: [Auth0],
+export const { handle, signIn } = SvelteKitAuth({
+	providers: [Auth0({ authorization: { params: { scope: 'openid profile offline_access' } } })],
 	callbacks: {
-		jwt({ token, account }) {
-			if (account?.id_token) {
-				return { ...token, accessToken: account.id_token };
+		async jwt({ token, account }) {
+			if (account) {
+				return {
+					...token,
+					accessToken: account.id_token,
+					refreshToken: account.refresh_token,
+					expiresAt: account.expires_at ? account.expires_at * 1000 : 0
+				};
+			} else if (typeof token.expiresAt === 'number' && token.expiresAt > Date.now()) {
+				return token;
+			} else {
+				if (!token.refreshToken) {
+					throw Error('Missing refresh token');
+				}
+				const url = new URL('/oauth/token', env.AUTH_AUTH0_ISSUER);
+				const response = await fetch(url.toString(), {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					},
+					body: new URLSearchParams({
+						grant_type: 'refresh_token',
+						client_id: env.AUTH_AUTH0_ID,
+						client_secret: env.AUTH_AUTH0_SECRET,
+						refresh_token: token.refreshToken as string
+					})
+				});
+				if (!response.ok) {
+					throw Error('Could not refresh token');
+				}
+				const result = await response.json();
+				return {
+					...token,
+					accessToken: result['id_token'],
+					expiresAt: Date.now() + result['expires_in'] * 1000
+				};
 			}
-			return token;
 		},
 		async session({ session, token }) {
 			if (typeof token.accessToken === 'string') {
