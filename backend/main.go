cea98aa2e74e73c8f0eff6b5c3894903eeab6d4e
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-03 19:40:00 -0300

    feat: swap google for auth0 auth middleware

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,7 @@ import (
 
 	"os"
 
-	"github.com/Jahn16/habitshare/middlewares/googleauth"
+	"github.com/Jahn16/habitshare/middlewares/auth0"
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/joho/godotenv"
@@ -18,7 +18,7 @@ func main() {
 	app := fiber.New()
 	db, _ := database.Setup()
 	godotenv.Load()
-	authMiddleware := googleauth.New(googleauth.Config{GoogleID: os.Getenv("AUTH_GOOGLE_ID")})
+	authMiddleware := auth0.New(auth0.Config{Issuer: os.Getenv("AUTH_AUTH0_DOMAIN"), Audience: []string{os.Getenv("AUTH_AUTH0_AUDIENCE")}})
 
 	app.Get("/habits", handlers.HabitList(db))
 	app.Get("/habits/:id", handlers.HabitGet(db))
