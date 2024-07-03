ffaf35ad9cce741c042a4993c3c56e2adc8796eb
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-03 19:34:31 -0300

    feat: swap google auth for auth0

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,5 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
+import Auth0 from '@auth/sveltekit/providers/auth0';
 // import { DefaultSession } from '@auth/core/types';
 
 declare module '@auth/sveltekit' {
@@ -9,7 +10,7 @@ declare module '@auth/sveltekit' {
 }
 
 export const { handle } = SvelteKitAuth({
-	providers: [Google],
+	providers: [Auth0],
 	callbacks: {
 		jwt({ token, account }) {
 			if (account?.id_token) {
