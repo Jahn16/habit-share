5eadab1c8ebcb945784804066b34a438e44c2b8d
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-03 23:17:57 -0300

    refactor: add submitForm function

--- frontend/src/components/record.svelte
+++ frontend/src/components/record.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import { Form } from '@sveltestrap/sveltestrap';
-
 	import type { HabitRecord } from '../models';
 	export let habitId: number;
 	export let date: string;
 	export let record: HabitRecord | undefined = undefined;
+
+	let form: HTMLFormElement;
+	const submitForm = () => {
+		if (form) {
+			form.submit();
+		}
+	};
 </script>
 
 {#if !record}
-	<Form method="post" action="?/record">
+	<form method="post" action="?/record" bind:this={form}>
 		<input type="hidden" value={habitId} name="habit-id" />
 		<input type="hidden" value={date} name="date" />
-		<input type="checkbox" onChange="this.form.submit()" />
-	</Form>
+		<input type="checkbox" on:change={submitForm} />
+	</form>
 {:else}
-	<Form method="post" action="?/undo">
+	<form method="post" action="?/undo" bind:this={form}>
 		<input type="hidden" value={record.ID} name="record-id" />
-		<input type="checkbox" onChange="this.form.submit()" checked />
-	</Form>
+		<input type="checkbox" on:change={submitForm} checked />
+	</form>
 {/if}
