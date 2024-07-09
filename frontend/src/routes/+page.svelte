bb11f74a2cd7450f45fd6ff6e328d20399dda4fe
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-08 23:07:04 -0300

    feat: add home page

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Container } from '@sveltestrap/sveltestrap';
+</script>
+
+<Container>
+	<h1>Social Habits</h1>
+	<br />
+	<h2>Why?</h2>
+	<br />
+	<p>
+		This project is heavily inspired by the printable habit trackers. I find it very powerful to be
+		able to see your habits in a single frame and all your successes and failures along the way.
+	</p>
+	<p>
+		Another key factor in building habits is to have good people by your side, always wishing you
+		the best. That's why this project is social at its core, you can share and talk about your
+		habits with your friends and family.
+	</p>
+</Container>
