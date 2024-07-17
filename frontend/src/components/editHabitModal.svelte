96551ac30611a89659b65c12f9058407f00543ea
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-17 20:59:14 -0300

    feat: add delete form to modal

--- frontend/src/components/editHabitModal.svelte
+++ frontend/src/components/editHabitModal.svelte
@@ -10,15 +10,25 @@
 	}
 
 	let habit: Habit;
+	let deleteForm: HTMLFormElement;
 </script>
 
 {#if habit}
 	<Modal body header="Editing habit: {habit.name}" isOpen={open} {toggle}>
+		<form method="POST" action="?/delete" bind:this={deleteForm}>
+			<input type="hidden" name="habit-id" value={habit.ID} />
+		</form>
 		<ModalFooter>
 			<Container>
 				<Row class="justify-content-between">
 					<Col sm="auto">
-						<Button color="danger" on:click={toggle}><Icon name="trash" /></Button>
+						<Button
+							color="danger"
+							on:click={() => {
+								toggle();
+								deleteForm.requestSubmit();
+							}}><Icon name="trash" /></Button
+						>
 					</Col>
 					<Col sm="auto">
 						<Button color="secondary" on:click={toggle}>Cancel</Button>
