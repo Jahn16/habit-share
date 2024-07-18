d154377979e2c6e4e8caed049affa650484f48fc
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-17 22:49:16 -0300

    feat: add edit habit form

--- frontend/src/components/editHabitModal.svelte
+++ frontend/src/components/editHabitModal.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-	import { Button, Col, Container, Icon, Modal, ModalFooter, Row } from '@sveltestrap/sveltestrap';
+	import {
+		Button,
+		Col,
+		Container,
+		Icon,
+		Input,
+		Modal,
+		ModalFooter,
+		Row
+	} from '@sveltestrap/sveltestrap';
 
 	import type { Habit } from '../models';
 	let open = false;
@@ -10,11 +19,37 @@
 	}
 
 	let habit: Habit;
+	let updateForm: HTMLFormElement;
 	let deleteForm: HTMLFormElement;
 </script>
 
 {#if habit}
 	<Modal body header="Editing habit: {habit.name}" isOpen={open} {toggle}>
+		<form method="POST" action="?/update" bind:this={updateForm}>
+			<input type="hidden" name="habit-id" value={habit.ID} />
+			<div class="mb-3">
+				<label for="edit-habit-name" class="form-label">Habit Name</label>
+				<Input
+					type="text"
+					class="form-control"
+					id="edit-habit-name"
+					name="habit-name"
+					value={habit.name}
+				/>
+			</div>
+			<div class="mb-3">
+				<label for="edit-habit-goal" class="form-label">Weekly Goal</label>
+				<Input type="select" id="edit-habit-goal" name="habit-goal" class="form-select">
+					{#each [1, 2, 3, 4, 5, 6, 7] as goal}
+						{#if goal === habit.goal}
+							<option selected>{goal}</option>
+						{:else}
+							<option>{goal}</option>
+						{/if}
+					{/each}
+				</Input>
+			</div>
+		</form>
 		<form method="POST" action="?/delete" bind:this={deleteForm}>
 			<input type="hidden" name="habit-id" value={habit.ID} />
 		</form>
@@ -31,7 +66,13 @@
 						>
 					</Col>
 					<Col sm="auto">
-						<Button color="secondary" on:click={toggle}>Cancel</Button>
+						<Button
+							color="primary"
+							on:click={() => {
+								toggle();
+								updateForm.requestSubmit();
+							}}><Icon name="save2" /></Button
+						>
 					</Col>
 				</Row>
 			</Container>
