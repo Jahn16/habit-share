40f60847beb42c1f2df4fe297212d4a517799879
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-22 19:32:36 -0300

    feat: add custom color to checkbox

--- frontend/src/components/record.svelte
+++ frontend/src/components/record.svelte
@@ -3,6 +3,7 @@
 	import { enhance } from '$app/forms';
 	import { Howl } from 'howler';
 
+	export let color: string = 'red';
 	export let habitId: number;
 	export let date: string;
 	export let record: HabitRecord | undefined = undefined;
@@ -16,34 +17,42 @@
 	};
 </script>
 
-{#if !record}
-	<form
-		method="post"
-		action="?/record"
-		bind:this={form}
-		use:enhance={() => {
-			notificationSound.play();
-			return async ({ update }) => {
-				update({ reset: false });
-			};
-		}}
-	>
-		<input type="hidden" value={habitId} name="habit-id" />
-		<input type="hidden" value={date} name="date" />
-		<input type="checkbox" on:change={submitForm} />
-	</form>
-{:else}
-	<form
-		method="post"
-		action="?/undo"
-		bind:this={form}
-		use:enhance={() => {
-			return async ({ update }) => {
-				update({ reset: false });
-			};
-		}}
-	>
-		<input type="hidden" value={record.ID} name="record-id" />
-		<input type="checkbox" on:change={submitForm} checked />
-	</form>
-{/if}
+<div style="--color: {color};">
+	{#if !record}
+		<form
+			method="post"
+			action="?/record"
+			bind:this={form}
+			use:enhance={() => {
+				notificationSound.play();
+				return async ({ update }) => {
+					update({ reset: false });
+				};
+			}}
+		>
+			<input type="hidden" value={habitId} name="habit-id" />
+			<input type="hidden" value={date} name="date" />
+			<input type="checkbox" on:change={submitForm} />
+		</form>
+	{:else}
+		<form
+			method="post"
+			action="?/undo"
+			bind:this={form}
+			use:enhance={() => {
+				return async ({ update }) => {
+					update({ reset: false });
+				};
+			}}
+		>
+			<input type="hidden" value={record.ID} name="record-id" />
+			<input type="checkbox" on:change={submitForm} checked />
+		</form>
+	{/if}
+</div>
+
+<style>
+	input[type='checkbox'] {
+		accent-color: var(--color);
+	}
+</style>
