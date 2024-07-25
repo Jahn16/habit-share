f70667f3f15e35200a53ecfd1f972975e683cebf
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-24 21:57:06 -0300

    fix: remove email from user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -10,7 +10,6 @@ type User struct {
 	gorm.Model
 	ID        string    `json:"string"`
 	Name      string    `json:"name"`
-	Email     string    `json:"email"`
 	Picture   string    `json:"picture"`
 	Habits    []Habit   `json:"habits"`
 	CreatedAt time.Time `json:"createdAt"`
