606710340f0697aff240d15e7b3b19a65c2ff036
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-24 21:12:12 -0300

    feat: add preferences page

---
+++ frontend/src/routes/preferences/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Col, Container, Form, FormGroup, Input, Label, Row } from '@sveltestrap/sveltestrap';
+	import palette from 'google-palette';
+	import palettes from '$lib/data/palettes.json';
+
+	const paletteKinds = Object.keys(palettes);
+	let paletteKind = 'sequential';
+	let colors: string[];
+	let paletteName = 'cb-Blues';
+
+	const updateColors = () => {
+		colors = palette(paletteName, 9, 0) || [];
+		colors.reverse();
+	};
+	updateColors();
+</script>
+
+<Container class="w-50">
+	<Form>
+		<FormGroup>
+			<Label>Palette Type</Label>
+			<Input type="select" bind:value={paletteKind}>
+				{#each paletteKinds as pk}
+					<option>{pk}</option>
+				{/each}
+			</Input>
+		</FormGroup>
+		<FormGroup>
+			<Label for="color-pallete">Color Pallete</Label>
+			<Input type="select" id="color-pallete" bind:value={paletteName} on:change={updateColors}>
+				{#each palettes[paletteKind] as p}
+					<option>{p}</option>
+				{/each}
+			</Input>
+		</FormGroup>
+	</Form>
+	<Row style="border: thin solid;">
+		{#each colors as color}
+			<Col class="color-square" style="width: 1em;height:1em;background-color: #{color};"></Col>
+		{/each}
+	</Row>
+</Container>
+
+<style>
+	.color-square {
+		height: 1em;
+		width: 1em;
+	}
+</style>
