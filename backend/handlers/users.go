2d4b79e36d7c38f97587494060598debd41f1779
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-25 20:10:50 -0300

    feat: add user update method

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -25,7 +25,6 @@ func UserCreate(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		userID := c.Locals("id").(string)
 		username := c.Locals("username").(string)
-		email := c.Locals("email").(string)
 		picture := c.Locals("picture").(string)
 		user := models.User{
 			ID:      userID,
@@ -38,6 +37,30 @@ func UserCreate(db *gorm.DB) fiber.Handler {
 			"value":  user,
 		})
 
+	}
+}
+func UserUpdate(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		userID := c.Locals("id").(string)
+		var user models.User
+		result := db.Preload("Habits.Records").First(&user, "id = ?", userID)
+		if result.Error != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"sucess": "false", "message": "User not found"})
+		}
+		updatedUser := new(models.User)
+		if err := c.BodyParser(updatedUser); err != nil {
+			return err
+		}
+		user.Name = updatedUser.Name
+		user.ColorPalette = updatedUser.ColorPalette
+		updateResult := db.Save(&user)
+		if updateResult.Error != nil {
+			return updateResult.Error
+		}
+		return c.JSON(fiber.Map{
+			"success": true,
+			"value":   user,
+		})
 	}
 }
 
